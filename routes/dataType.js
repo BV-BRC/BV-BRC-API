105225f6baf96d94ee8befdda7f820f4d03d38af
Harry Yoo <hyun@vbi.vt.edu>
2015-04-13 15:50:39 -0400

    added core that does not contain ACL fields

--- routes/dataType.js
+++ routes/dataType.js
@@ -66,7 +66,7 @@ var decorateQuery = function(req, res, next) {
 
 	debug("decorateQuery", req.solr_query);
 	req.call_params[0] = req.call_params[0] || "&q=*:*";
-	var publicFree=['enzyme_class_ref', 'gene_ontology_ref', 'id_ref', 'protein_family_ref', 'misc_niaid_gsc', 'taxonomy'];
+	var publicFree=['enzyme_class_ref', 'gene_ontology_ref', 'id_ref', 'misc_niaid_gsc', 'pathway_ref', 'ppi', 'protein_family_ref', 'sp_gene_evidence', 'sp_gene_ref', 'taxonomy', 'transcriptomics_experiment', 'transcriptomics_gene', 'transcriptomics_sample'];
 	if (!req.user) {
 		if (publicFree.indexOf(req.call_collection)<0) {
 			req.call_params[0] = req.call_params[0] + "&fq=public:true"
