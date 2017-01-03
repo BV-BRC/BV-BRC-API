f8d7a1358d71fa47932b77daf804a7883309d48f
Harry Yoo <hsyoo@anl.gov>
2017-01-03 13:54:31 -0600

    implement descendents function for backward compatibility

--- ExpandingQuery.js
+++ ExpandingQuery.js
@@ -161,6 +161,12 @@ var LazyWalk = exports.LazyWalk = function(term, opts){
 							debug("Error in sub query", err);
 							return "(NOT_A_VALID_ID)";
 						});
+					}else if(term.name == "descendants" && term.args.length == 1){
+						// debug("call descendants(): ", term.args);
+						var taxId = term.args[0];
+						var p1 = encodeURIComponent('(*,' + taxId + ')');
+						var p2 = encodeURIComponent('(*,' + taxId + ',*)');
+						return "or(eq(taxpath_a," + p1 + "),eq(taxpath_a," + p2 + "),eq(taxpath_b," + p1 + "),eq(taxpath_b," + p2 + "))";
 					}else if(term.name == "GenomeGroup"){
 //							debug("call getWorkspaceObject(): ", term.args[0]);
 						return when(getWorkspaceObject(term.args[0], opts), function(ids){
