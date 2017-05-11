cb80cbc3ee09df0de7ff99a52bcabc941a04b69f
Zane Reynolds <zreyn@ndssl-zreyn-thunderbolt.bioinformatics.vt.edu>
2017-05-11 09:58:41 -0400

    adjusted idList and listIdentifier to correspond to transcriptomics_sample and adjusted result limits

--- routes/hpiSearch.js
+++ routes/hpiSearch.js
@@ -11,15 +11,14 @@ might be of interest (i.e. yield a similar set of results)
 GETs
 curl 'http://localhost:3001/hpi/search'
 curl 'http://localhost:3001/hpi/search/experiment'
-curl 'http://localhost:3001/hpi/search/experiment/543'
-curl 'http://localhost:3001/hpi/search/experiment/543/idList'
-curl 'http://localhost:3001/hpi/search/experiment/543/idList/543,21'
-curl 'http://localhost:3001/hpi/search/experiment/543/idList/543,21/ids'
-curl 'http://localhost:3001/hpi/search/experiment/543/idList/543,21/ids?includeOrthologs="human"'
+curl 'http://localhost:3001/hpi/search/experiment/GSE79731'
+curl 'http://localhost:3001/hpi/search/experiment/GSE79731/idList/100000211'
+curl 'http://localhost:3001/hpi/search/experiment/GSE79731/idList/100000211/ids'
+curl 'http://localhost:3001/hpi/search/experiment/GSE79731/idList/100000211/ids?includeOrthologs='human''
 curl 'http://localhost:3001/hpi/search/api'
 
 POST
-curl -H "Content-Type: application/json" -X POST "http://localhost:3001/hpi/search" -d '{ "type": "input string", "idSource": "id source input string", "ids": ["id 1", "id 2", "id 3"], "threshold": 0.5, "thresholdType": "a number", "additionalFlags": { "key1": "value1", "key2": "value2" } }'
+curl -H 'Content-Type: application/json' -X POST 'http://localhost:3001/hpi/search' -d '{ 'type': 'input string', 'idSource': 'id source input string', 'ids': ['id 1', 'id 2', 'id 3'], 'threshold': 0.5, 'thresholdType': 'a number', 'organism': 'organism name', 'additionalFlags': { 'key1': 'value1', 'key2': 'value2' } }'
 
 */
 
@@ -27,63 +26,127 @@ curl -H "Content-Type: application/json" -X POST "http://localhost:3001/hpi/sear
 // import dependencies
 var express = require('express');
 var router = express.Router({strict: true, mergeParams: true});
-var defer = require("promised-io/promise").defer;
-var when = require("promised-io/promise").when;
-var config = require("../config");
-var bodyParser = require("body-parser");
+var defer = require('promised-io/promise').defer;
+var when = require('promised-io/promise').when;
+var config = require('../config');
+var bodyParser = require('body-parser');
 var debug = require('debug')('p3api-server:route/hpiSearchRouter');
-var httpParams = require("../middleware/http-params"); // checks for stuff starting with http_ in the query and sets it as a header
-var authMiddleware = require("../middleware/auth");
-var querystring = require("querystring");
-var RQLQueryParser = require("../middleware/RQLQueryParser");
-var DecorateQuery = require("../middleware/DecorateQuery");
-var APIMethodHandler = require("../middleware/APIMethodHandler");
-var Limiter = require("../middleware/Limiter");
+var httpParams = require('../middleware/http-params'); // checks for stuff starting with http_ in the query and sets it as a header
+var authMiddleware = require('../middleware/auth');
+var querystring = require('querystring');
+var RQLQueryParser = require('../middleware/RQLQueryParser');
+var APIMethodHandler = require('../middleware/APIMethodHandler');
+var Limiter = require('../middleware/Limiter');
 
 router.use(httpParams);
 router.use(authMiddleware);
 
 // handle GET hpi/search/
 // Not sure what to return here
-router.get("/", [
+router.get('/', [
 	bodyParser.urlencoded({extended: true}),
 	function(req, res, next){
-		res.write("--- acknowledged GET for hpi/search \n");
+		res.write('--- acknowledged GET for hpi/search \n');
     res.end();
 	}
 ])
 
 // POST hpi/search/
 // Given an input set of Host IDs and match parameters, return matching experiments and ID lists
-// curl -H "Content-Type: application/json" -X POST "http://localhost:3001/hpi/search" -d '{ "type": "input string", "idSource": "id source input string", "ids": ["id 1", "id 2", "id 3"], "threshold": 0.5, "thresholdType": "a number", "additionalFlags": { "key1": "value1", "key2": "value2" } }'
-router.post("/", [
+// curl -H 'Content-Type: application/json' -X POST 'http://localhost:3001/hpi/search' -d '{ 'type': 'input string', 'idSource': 'id source input string', 'ids': ['id 1', 'id 2', 'id 3'], 'threshold': 0.5, 'thresholdType': 'a number', 'organism': 'organism name', 'additionalFlags': { 'key1': 'value1', 'key2': 'value2' } }'
+router.post('/', [
 	bodyParser.json(),
 	function(req, res, next){
-    debug("req.body: ", req.body);
+    debug('req.body: ', req.body);
     // req.body.type
     // req.body.idSource
     // req.body.ids
     // req.body.threshold
     // req.body.thresholdType
+    // req.body.organism
     // req.body.additionalFlags
-    res.write("--- acknowledged POST for hpi/search \n");
+
+    req.call_collection = 'transcriptomics_gene';
+		req.call_method = 'query';
+
+    // accept a list of pids or feature_ids
+    if (!isNaN(req.params.id_list) && parseInt(Number(req.params.id_list)) == req.body.ids.id_list) {
+      req.call_params = ['&q=pid:(*' + req.body.ids.replace(',', '*+OR+*') + '*)'];
+    } else {
+      req.call_params = ['&q=feature_id:(*' + req.body.ids.replace(',', '*+OR+*') + '*)'];
+    }
+
+    // (uniprot id types) -- map to patric feature_id
+    // refseq_loc_tag
+    // ensembl_id
+    // feature_id
+    //
+
+		req.queryType = 'solr';
+		next();
+	},
+  Limiter,
+	APIMethodHandler,
+	function(req, res, next){
+
+
+
+    if(res.results && res.results.response && res.results.response.docs){
+      //
+      // var experiments = res.results.response.docs.map(function(d){
+			// 	return {
+      //     eid: d.eid,
+      //     accession:
+      //     'experimentIdentifier': d.eid, // String
+      //     'displayName': d.accession, // String
+      //     'type': , 'transcriptomics' // String (e.g. differential expression, genetic screen)
+      //     'description': , d.description, // String (free text)
+      //     'uri': 'https://www.patricbrc.org/view/ExperimentComparison/'+d.eid, // String (link to experiment page in BRC)
+      //     'species': d.organism, // String (TBD: is this a controlled vocabulary?)
+      //     'genomeVersion': '', // String (TBD: is this a controlled vocabulary?)
+      //     'validIdCount': 0, // Number (number of IDs that could be mapped to this organism)
+      //     'experimentSignificance': 0.0, // Number (overall significance for this experiment- meaning determined by type)
+      //     'significanceType': req.body.thresholdType, // String
+      //     'idLists': []
+      //   }
+      //       {
+      //         'listIdentifier': String,
+      //         'displayName': String,
+      //         'description': String,
+      //         'uri': String (link to list page in BRC.  optional; set to null if unavailable),
+      //         'type': String (see 'inputTypes' in API above),
+      //         'provenance': String
+      //         'significance': Number (meaning TBD) Note that significanceType moved to experiment level
+      //       }
+      //     ]
+      //
+      //   }
+			// });
+      //
+      //
+			// res.write(JSON.stringify(experiments));
+      res.write(JSON.stringify(res.results.response.docs));
+
+		}else{
+      res.write('--- acknowledged POST for hpi/search \n');
+    }
     res.end();
 	}
 ])
 
 // GET hpi/search/experiment
 // Maybe a 404 or a list of all experiment ids
-router.get("/experiment", [
+router.get('/experiment', [
   bodyParser.urlencoded({extended: true}),
   function(req, res, next){
-		req.call_collection = "transcriptomics_experiment";
-		req.call_method = "query";
-		req.call_params = ["&q=*:*"]; // default is 25 rows; add &rows=1000 to adjust
-		req.queryType = "solr";
+		req.call_collection = 'transcriptomics_experiment';
+		req.call_method = 'query';
+		req.call_params = ['&q=*:*&rows=25000']; // default is 25 rows; add &rows=1000 to adjust
+		req.queryType = 'solr';
 		next();
 	},
   //DecorateQuery,
-	Limiter,
+	//Limiter,
 	APIMethodHandler,
 	function(req, res, next){
 
@@ -95,7 +158,7 @@ router.get("/experiment", [
 			});
 			res.json(experiments);
 		}else{
-      res.write("--- acknowledged GET for hpi/search/experiemnt \n");
+      res.write('--- acknowledged GET for hpi/search/experiemnt \n');
     }
     res.end();
 
@@ -104,31 +167,31 @@ router.get("/experiment", [
 
 // GET hpi/search/experiment/{experimentIdentifier}
 // The details of an experiment, as showin in the primary endpoint
-router.get("/experiment/:id", [
+router.get('/experiment/:id', [
   bodyParser.urlencoded({extended: true}),
   function(req, res, next){
-		req.call_collection = "transcriptomics_experiment";
-		req.call_method = "query";
+		req.call_collection = 'transcriptomics_experiment';
+		req.call_method = 'query';
 
     // check if the id is a number or not (number=eid, not=accession)
     if (!isNaN(req.params.id) && parseInt(Number(req.params.id)) == req.params.id) {
-      req.call_params = ["&q=eid:" + req.params.id];
+      req.call_params = ['&q=eid:' + req.params.id];
     } else {
-      req.call_params = ["&q=accession:" + req.params.id];
+      req.call_params = ['&q=accession:' + req.params.id];
     }
 
-		req.queryType = "solr";
+		req.queryType = 'solr';
 		next();
 	},
 	//DecorateQuery,
-	Limiter,
+	//Limiter,
 	APIMethodHandler,
 	function(req, res, next){
 
     if(res.results && res.results.response && res.results.response.docs){
 			res.json(res.results.response.docs);
 		}else{
-      res.write("--- acknowledged GET for hpi/search/experiemnt/{experimentIdentifier} \n");
+      res.write('--- acknowledged GET for hpi/search/experiemnt/{experimentIdentifier} \n');
     }
     res.end();
 
@@ -137,80 +200,76 @@ router.get("/experiment/:id", [
 
 // GET hpi/search/experiment/{experimentIdentifier}/idList/{listIdentifier}
 // The details of an experiment, as shown in the primary endpoint
-router.get("/experiment/:id/idList/:id_list", [
+router.get('/experiment/:id/idList/:id_list', [
   bodyParser.urlencoded({extended: true}),
 	function(req, res, next){
-    req.call_collection = "transcriptomics_sample";
-		req.call_method = "query";
+    req.call_collection = 'transcriptomics_sample';
+		req.call_method = 'query';
 
     // check if the id is a number or not (number=eid, not=accession)
     if (!isNaN(req.params.id) && parseInt(Number(req.params.id)) == req.params.id) {
-      req.call_params = ["&q=eid:" + req.params.id];
+      req.call_params = ['&q=eid:' + req.params.id];
     } else {
-      req.call_params = ["&q=accession:" + req.params.id];
+      req.call_params = ['&q=accession:' + req.params.id];
     }
 
-    // accept either a list of accessions or sample ids
-    if (!isNaN(req.params.id_list) && parseInt(Number(req.params.id_list)) == req.params.id_list) {
-      // this was a sample id (which has a gene list)
-      req.call_params[0] = req.call_params[0] + ["+AND+pid:" + req.params.id_list];
-    } else {
-      // put together the id_list portion of the query string 'samples:(*GSM432865* OR *GSM432861*)'
-      req.call_params[0] = req.call_params[0] + "+AND+samples:(*" + req.params.id_list.replace(',', '*+OR+*') + "*)";
-    }    
+    // accept a sample id (which corresponds to a list of genes)
+    req.call_params[0] = req.call_params[0] + ['+AND+pid:' + req.params.id_list];
 
-		req.queryType = "solr";
+		req.queryType = 'solr';
 		next();
 
 	},
-  //DecorateQuery
   Limiter,
   APIMethodHandler,
   function(req, res, next){
     if(res.results && res.results.response && res.results.response.docs){
 			res.json(res.results.response.docs);
 		}else{
-      res.write("--- acknowledged GET for hpi/search/experiemnt/{experimentIdentifier}/idList/{listIdentifier} \n");
+      res.write('--- acknowledged GET for hpi/search/experiemnt/{experimentIdentifier}/idList/{listIdentifier} \n');
     }
     res.end();
   }
 ]);
 
-// GET hpi/search/experiment/{experimentIdentifier}/idList/{listIdentifier}/ids<?includeOrthologs="human">
+// GET hpi/search/experiment/{experimentIdentifier}/idList/{listIdentifier}/ids<?includeOrthologs='human'>
 // Return the ids for the idList.  If the optional includeOrthologs parameter is supplied,
 // return a second column with lorthologous ids from that organism
-router.get("/experiment/:id/idList/:id_list/ids", [
+router.get('/experiment/:id/idList/:id_list/ids', [
   bodyParser.urlencoded({extended: true}),
 	function(req, res, next){
-    req.call_collection = "transcriptomics_sample";
-    req.call_method = "query";
+    req.call_collection = 'transcriptomics_gene';
+    req.call_method = 'query';
 
     // check if the id is a number or not (number=eid, not=accession)
     if (!isNaN(req.params.id) && parseInt(Number(req.params.id)) == req.params.id) {
-      req.call_params = ["&q=eid:" + req.params.id];
+      req.call_params = ['&q=eid:' + req.params.id];
     } else {
-      req.call_params = ["&q=accession:" + req.params.id];
+      req.call_params = ['&q=accession:' + req.params.id];
     }
 
-    // put together the id_list portion of the query string 'samples:(*GSM432865* OR *GSM432861*)'
-    req.call_params[0] = req.call_params[0] + "+AND+samples:(*" + req.params.id_list.replace(',', '*+OR+*') + "*)";
+    // accept a sample id (which corresponds to a list of genes)
+    req.call_params[0] = req.call_params[0] + ['+AND+pid:' + req.params.id_list];
 
-    req.queryType = "solr";
+    // set the row limit
+    req.call_params[0] = req.call_params[0] + ['&rows=25000'];
+
+    req.queryType = 'solr';
     next();
 	},
-  //DecorateQuery
   Limiter,
   APIMethodHandler,
   function(req, res, next){
     if(res.results && res.results.response && res.results.response.docs){
       var samples = res.results.response.docs.map(function(d){
 				return {
-					pid: d.pid
+					patric_id: d.feature_id,
+          alt_locus_tag: d.alt_locus_tag
 				}
 			});
 			res.json(samples);
 		}else{
-      res.write("--- acknowledged GET for hpi/search/experiemnt/{experimentIdentifier}/idList/{listIdentifier}/ids \n");
+      res.write('--- acknowledged GET for hpi/search/experiemnt/{experimentIdentifier}/idList/{listIdentifier}/ids \n');
     }
     res.end();
   }
@@ -218,7 +277,7 @@ router.get("/experiment/:id/idList/:id_list/ids", [
 
 // GET hpi/search/api
 // Supplies information specific to this BRC's implementation of the API
-router.get("/api", [
+router.get('/api', [
   bodyParser.urlencoded({extended: true}),
 	function(req, res, next){
 
@@ -232,7 +291,7 @@ router.get("/api", [
       'name': 'genes',
       'displayName': 'Gene List',
       'description': 'A list of genes to match against experiments',
-      'idSources': ['NCBI GEO Accession', 'PATRIC'],
+      'idSources': ['PATRIC', 'Alt Locus Tag'],
       'thresholdTypes': [{
         'name': 'percent_matched',
         'displayName': 'Percent Matched',
