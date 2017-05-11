e45e9e49877031adf03c9356a3c853b447a3824f
Zane Reynolds <zreyn@ndssl-zreyn-thunderbolt.bioinformatics.vt.edu>
2017-05-11 16:46:41 -0400

    stopping work for the day commit

--- routes/hpiSearch.js
+++ routes/hpiSearch.js
@@ -18,7 +18,7 @@ curl 'http://localhost:3001/hpi/search/experiment/GSE79731/idList/100000211/ids?
 curl 'http://localhost:3001/hpi/search/api'
 
 POST
-curl -H 'Content-Type: application/json' -X POST 'http://localhost:3001/hpi/search' -d '{ 'type': 'input string', 'idSource': 'id source input string', 'ids': ['id 1', 'id 2', 'id 3'], 'threshold': 0.5, 'thresholdType': 'a number', 'organism': 'organism name', 'additionalFlags': { 'key1': 'value1', 'key2': 'value2' } }'
+curl -H 'Content-Type: application/json' -X POST 'http://localhost:3001/hpi/search' -d '{ "type": "gene", "idSource": "alt_locus_tag", "ids": ["NP_031402.3", "XP_011246971.1"], "threshold": 0.5, "thresholdType": "percent_matched", "organism": "Mus musculus", "additionalFlags": { "useOrthology": "false" } }'
 
 */
 
@@ -38,6 +38,17 @@ var RQLQueryParser = require('../middleware/RQLQueryParser');
 var APIMethodHandler = require('../middleware/APIMethodHandler');
 var Limiter = require('../middleware/Limiter');
 
+// constants and magic words
+const INPUT_TYPE_GENE = 'gene';
+const ID_SOURCE_PATRIC = 'patric';
+const ID_SOURCE_ALT_LOCUS_TAG = 'alt_locus_tag';
+const THRESHOLD_PERCENT = 'percent_matched';
+const THRESHOLD_LOG_RATIO = 'log_ratio';
+const FLAG_ORTHOLOGY = 'useOrthology';
+
+var PATRIC_URL = config.get("distributeURL");
+
+
 router.use(httpParams);
 router.use(authMiddleware);
 
@@ -49,13 +60,15 @@ router.get('/', [
 		res.write('--- acknowledged GET for hpi/search \n');
     res.end();
 	}
-])
+]);
 
 // POST hpi/search/
 // Given an input set of Host IDs and match parameters, return matching experiments and ID lists
-// curl -H 'Content-Type: application/json' -X POST 'http://localhost:3001/hpi/search' -d '{ 'type': 'input string', 'idSource': 'id source input string', 'ids': ['id 1', 'id 2', 'id 3'], 'threshold': 0.5, 'thresholdType': 'a number', 'organism': 'organism name', 'additionalFlags': { 'key1': 'value1', 'key2': 'value2' } }'
+// curl -H 'Content-Type: application/json' -X POST 'http://localhost:3001/hpi/search' -d '{ "type": "gene", "idSource": "alt_locus_tag", "ids": ["NP_031402.3", "XP_011246971.1"], "threshold": 0.5, "thresholdType": "percent_matched", "organism": "Mus musculus", "additionalFlags": { "useOrthology": "false" } }'
 router.post('/', [
 	bodyParser.json(),
+
+  // Step 1: Assemble the list of matching genes
 	function(req, res, next){
     debug('req.body: ', req.body);
     // req.body.type
@@ -69,70 +82,212 @@ router.post('/', [
     req.call_collection = 'transcriptomics_gene';
 		req.call_method = 'query';
 
-    // accept a list of pids or feature_ids
-    if (!isNaN(req.params.id_list) && parseInt(Number(req.params.id_list)) == req.body.ids.id_list) {
-      req.call_params = ['&q=pid:(*' + req.body.ids.replace(',', '*+OR+*') + '*)'];
-    } else {
-      req.call_params = ['&q=feature_id:(*' + req.body.ids.replace(',', '*+OR+*') + '*)'];
+    if (req.body.type != INPUT_TYPE_GENE){
+      // return 422
     }
 
-    // (uniprot id types) -- map to patric feature_id
-    // refseq_loc_tag
-    // ensembl_id
-    // feature_id
-    //
+    var query = [];
+    // accept a list of feature_ids or alt_locus_tags
+    switch(req.body.idSource){
+			case ID_SOURCE_PATRIC:
+        query.push('&q=feature_id:');
+        break;
+      case ID_SOURCE_ALT_LOCUS_TAG:
+        query.push('&q=alt_locus_tag:');
+        break;
+      default:
+        // return 422
+        break;
+    }
 
+    // add the ids to the query
+    query.push('(' + req.body.ids.join('+OR+') + ')');
+
+    // facet on sample
+    query.push('&facet.field=pid&facet.mincount=1&facet=on');
+
+    // increase row limit
+    query.push('&rows=25000');
+
+    req.call_params = [query.join('')];
 		req.queryType = 'solr';
+
 		next();
 	},
   Limiter,
 	APIMethodHandler,
+
+  // Step 2: Gather the metadata for the matching samples
 	function(req, res, next){
 
+    // ensure we got everything we expected in the response
+    if (res.results && res.results.response && res.results.facet_counts &&
+      res.results.facet_counts.facet_fields && res.results.facet_counts.facet_fields.pid) {
+
+      req.call_collection = 'transcriptomics_sample';
+  		req.call_method = 'query';
+
+      var query = [];
+
+      // add the ids to the query
+      var pids = [];
+      for (var i in res.results.facet_counts.facet_fields.pid){
+        if(i % 2 === 0) { // index is even
+          pids.push(res.results.facet_counts.facet_fields.pid[i]);
+        }
+      }
+      query.push('&q=pid:(' + pids.join('+OR+') + ')');
+
+      // facet on experiment
+      query.push('&facet.field=eid&facet.mincount=1&facet=on');
+
+      // increase row limit
+      query.push('&rows=25000');
+
+      req.call_params = [query.join('')];
+  		req.queryType = 'solr';
+
+      // // stash the genes in the request
+      // req.genes = res.results.response.docs;
+
+      next();
+    } else {
+      // no results XXX bail?
+      res.write('--- acknowledged POST for hpi/search \n');
+      res.end();
+    }
 
 
-    if(res.results && res.results.response && res.results.response.docs){
+	},
+  Limiter,
+  APIMethodHandler,
+
+  // Step 3: Gather the metadata for the matching experiments
+  function(req, res, next){
+
+    if (res.results && res.results.response && res.results.response.docs) {
+
+      // stash the samples in the request
+      req.samples = res.results.response.docs;
+
+      // prepare the query for experiments
+      req.call_collection = 'transcriptomics_experiment';
+      req.call_method = 'query';
+
+      var query = [];
+
+      // add the ids to the query
+      var eids = [];
+      for (var i in res.results.facet_counts.facet_fields.eid){
+        if(i % 2 === 0) { // index is even
+          eids.push(res.results.facet_counts.facet_fields.eid[i]);
+        }
+      }
+      query.push('&q=eid:(' + eids.join('+OR+') + ')');
+
+      // increase row limit
+      query.push('&rows=25000');
+
+      req.call_params = [query.join('')];
+      req.queryType = 'solr';
+
+      next();
+		}else{
+      res.write('--- acknowledged POST for hpi/search \n');
+      res.end();
+    }
+
+  },
+  Limiter,
+  APIMethodHandler,
+
+  // Step 4: Prepare the final response
+  // Note: it would have been better to just return sample and experiment ids
+  //       instead of putting this whole response together, since we have APIs
+  //       to get the metadata elsewhere.
+  function(req, res, next){
+
+    if (res.results && res.results.response && res.results.response.docs) {
+
+      // prepare the output for the experiments
+      var exp_map = [];
+      for (i in res.results.response.docs){
+        var exp = res.results.response.docs[i];
+        exp_map.push({exp.eid, exp.map(function(d){
+          return {
+            experimentIdentifier: d.accession,
+            displayName: d.title,
+            type: 'transcriptomics',
+            description: d.description,
+            uri: PATRIC_URL + '/view/ExperimentComparison/' + d.eid,
+            species: d.organism,
+            genomeVersion: d.genome_ids,
+            validIdCount: 'TBD',
+            experimentSignificance: 0.0,
+            significanceType: req.body.thresholdType,
+            idLists: []
+          }
+        })});
+      }
+
       //
       // var experiments = res.results.response.docs.map(function(d){
-			// 	return {
-      //     eid: d.eid,
-      //     accession:
-      //     'experimentIdentifier': d.eid, // String
-      //     'displayName': d.accession, // String
-      //     'type': , 'transcriptomics' // String (e.g. differential expression, genetic screen)
-      //     'description': , d.description, // String (free text)
-      //     'uri': 'https://www.patricbrc.org/view/ExperimentComparison/'+d.eid, // String (link to experiment page in BRC)
-      //     'species': d.organism, // String (TBD: is this a controlled vocabulary?)
-      //     'genomeVersion': '', // String (TBD: is this a controlled vocabulary?)
-      //     'validIdCount': 0, // Number (number of IDs that could be mapped to this organism)
-      //     'experimentSignificance': 0.0, // Number (overall significance for this experiment- meaning determined by type)
-      //     'significanceType': req.body.thresholdType, // String
-      //     'idLists': []
+      //   return {
+      //     experimentIdentifier: d.accession,
+      //     displayName: d.title,
+      //     type: 'transcriptomics',
+      //     description: d.description,
+      //     uri: PATRIC_URL + '/view/ExperimentComparison/' + d.eid,
+      //     species: d.organism,
+      //     genomeVersion: d.genome_ids,
+      //     validIdCount: 'TBD',
+      //     experimentSignificance: 0.0,
+      //     significanceType: req.body.thresholdType,
+      //     idLists: []
       //   }
-      //       {
-      //         'listIdentifier': String,
-      //         'displayName': String,
-      //         'description': String,
-      //         'uri': String (link to list page in BRC.  optional; set to null if unavailable),
-      //         'type': String (see 'inputTypes' in API above),
-      //         'provenance': String
-      //         'significance': Number (meaning TBD) Note that significanceType moved to experiment level
-      //       }
-      //     ]
+      // });
       //
-      //   }
-			// });
-      //
-      //
-			// res.write(JSON.stringify(experiments));
-      res.write(JSON.stringify(res.results.response.docs));
 
-		}else{
+      // prepare and attach samples to experiments
+      for (j in req.samples){
+        var sample = req.samples[j];
+        exp_map[sample.eid].idLists.push(
+          sample.map(function(d){
+            return {
+              listIdentifier: d.pid,
+              displayName: d.expname,
+              description: d.expname,
+              uri: PATRIC_URL + '/view/ExperimentComparison/' + d.eid + '#view_tab=comparisons',
+              type: INPUT_TYPE_GENE,
+              provenance: 'TBD',
+              significance: 'TBD'
+            }
+          }
+        ));
+      }
+
+      // // prepare the samples and attach them to the experiments
+      // var samples = req.samples.map(function(d){
+      //   return {
+      //     listIdentifier: d.pid,
+      //     displayName: d.expname,
+      //     description: d.expname,
+      //     uri: PATRIC_URL + '/view/ExperimentComparison/' + d.eid + '#view_tab=comparisons',
+      //     type: INPUT_TYPE_GENE,
+      //     provenance: 'TBD',
+      //     significance: 'TBD'
+      //   }
+      // });
+
+      // output the experiments
+      res.write(JSON.stringify(exp_map.values()));
+
+    }else{
       res.write('--- acknowledged POST for hpi/search \n');
     }
     res.end();
-	}
-])
+  }
+]);
 
 // GET hpi/search/experiment
 // Maybe a 404 or a list of all experiment ids
@@ -288,25 +443,25 @@ router.get('/api', [
 
     // for each input type, provide the necessary info
     var gene_input = {
-      'name': 'genes',
+      'name': INPUT_TYPE_GENE,
       'displayName': 'Gene List',
       'description': 'A list of genes to match against experiments',
-      'idSources': ['PATRIC', 'Alt Locus Tag'],
+      'idSources': [ID_SOURCE_PATRIC, ID_SOURCE_ALT_LOCUS_TAG],
       'thresholdTypes': [{
-        'name': 'percent_matched',
+        'name': THRESHOLD_PERCENT,
         'displayName': 'Percent Matched',
         'description': 'Percent of provided genes matched to the genes in an experiment',
         'min': 0.0,
         'max': 100.0
       },{
-        'name': 'log_ratio',
+        'name': THRESHOLD_LOG_RATIO,
         'displayName': 'Log Ratio',
         'description': 'A differential expression value specified as log2 (test/control)',
         'min': -5.0,
         'max': 5.0
       }],
       'additionalFlags': [{
-        'key':'useOrthology',
+        'key':FLAG_ORTHOLOGY,
         'jsonType':'boolean',
         'description':'If the useOrthology flag is set, returns a second column with orhologous IDs from that organism.'}]
 
