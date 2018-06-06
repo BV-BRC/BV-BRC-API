2ebf8043ef115915c45c086f7355a8e3e70fc092
Hyunseung Yoo <hsyoo@anl.gov>
2018-06-06 15:05:40 -0500

    remove unused code

--- rpc.js
+++ rpc.js
@@ -1,13 +1,11 @@
 const MSA = require('./rpc/msa')
 const Cluster = require('./rpc/cluster')
 const ProteinFamily = require('./rpc/proteinFamily')
-const Subsystem = require('./rpc/subSystem.js')
 const TranscriptomicsGene = require('./rpc/transcriptomicsGene')
 
 module.exports = {
   'multipleSequenceAlignment': MSA,
   'proteinFamily': ProteinFamily,
-  'subSystem': Subsystem,
   'transcriptomicsGene': TranscriptomicsGene,
   'cluster': Cluster
 }
