eede9aecb781769076fe5fa090fdb4b7fe61bdaf
Hyunseung Yoo <hsyoo@anl.gov>
2018-07-23 16:14:08 -0500

    catch RQL parsing error and send 400 response

--- middleware/RQLQueryParser.js
+++ middleware/RQLQueryParser.js
@@ -8,17 +8,23 @@ module.exports = function (req, res, next) {
   if (req.queryType === 'rql') {
     req.call_params[0] = req.call_params[0] || ''
     // debug("Orig Query: ", req.call_params[0]);
-    when(Expander.ResolveQuery(req.call_params[0], {req: req, res: res}), function (q) {
-      debug('Resolved Query: ', q)
-      if (q === '()') { q = '' }
-      var rq = rql(q)
-      const max = (req.isDownload) ? 999999999 : 25000
-
-      req.call_params[0] = rq.toSolr({maxRequestLimit: max, defaultLimit: 25})
-      // debug("Converted Solr Query: ", req.call_params[0]);
-      req.queryType = 'solr'
-      next()
-    })
+    try {
+      // catch parsing errors
+      when(Expander.ResolveQuery(req.call_params[0], {req: req, res: res}), function (q) {
+        debug('Resolved Query: ', q)
+        if (q === '()') { q = '' }
+        var rq = rql(q)
+        const max = (req.isDownload) ? 999999999 : 25000
+        req.call_params[0] = rq.toSolr({maxRequestLimit: max, defaultLimit: 25})
+        // debug("Converted Solr Query: ", req.call_params[0]);
+        req.queryType = 'solr'
+        next()
+      })
+    } catch (err) {
+      // throw 400
+      debug(`400 ERROR: ${err.message}`)
+      res.status(400).send(err.message)
+    }
   } else {
     next()
   }
