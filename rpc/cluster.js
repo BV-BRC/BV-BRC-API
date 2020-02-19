7926bf862ad87a6b5b8cb81c54bf9eb09bfa7df5
nconrad <1194246+nconrad@users.noreply.github.com>
2020-02-19 17:55:16 -0600

    increase timeout and kill clustering after 5 mins (instead of 20); PATRIC3/patric3_website#2340

--- rpc/cluster.js
+++ rpc/cluster.js
@@ -5,26 +5,28 @@ const spawn = require('child_process').spawn
 const Temp = require('temp')
 const fs = require('fs-extra')
 
+let child
+
 function runCluster (data, config, opts) {
   const def = new Deferred()
   let errorClosed
 
   debug('Run Cluster')
 
-  const tempFileInput = Temp.path({prefix: 'cluster.', suffix: '.input'})
+  const tempFileInput = Temp.path({ prefix: 'cluster.', suffix: '.input' })
   const tempFileBase = tempFileInput.replace('.input', '')
   const tempFileOutput = tempFileBase + '.cdt'
   const tempFilePath = tempFileBase.split('cluster.')[0]
 
   debug('Cluster Temp File Input: ', tempFileInput, 'at', tempFilePath)
 
-  fs.outputFile(tempFileInput, data, function (err) {
+  fs.outputFile(tempFileInput, data, (err) => {
     if (err) {
       def.reject('Unable to write input data to', tempFileInput)
       return
     }
 
-    const child = spawn('cluster',
+    child = spawn('cluster',
       ['-f', tempFileInput, '-u', tempFileBase,
         '-g', config.g || 1, '-e', config.e || 2, '-m', config.m || 'a'],
       {
@@ -36,18 +38,18 @@ function runCluster (data, config, opts) {
         ]
       })
 
-    setTimeout(function () {
+    setTimeout(() => {
       if (child.exitCode === 0) {
         debug(`child process ${child.pid} is finished normally`)
       } else {
         debug('Cluster timed out!')
         errorClosed = true
-        def.reject('Timed out. Cluster took more than 20 mins. Please reduce the data set and try again.')
+        def.reject('Timed out. Cluster took more than 5 mins. Please reduce the data set and try again.')
         child.kill('SIGHUP')
       }
-    }, 1000 * 60 * 20)
+    }, 1000 * 60 * 5)
 
-    child.stderr.on('data', function (errData) {
+    child.stderr.on('data', (errData) => {
       debug('Cluster STDERR Data: ', errData.toString())
     })
 
@@ -56,12 +58,12 @@ function runCluster (data, config, opts) {
       def.reject(err)
     })
 
-    child.on('close', function (code) {
+    child.on('close', (code) => {
       debug('Cluster Process closed.', code)
 
       if (!errorClosed) {
         // read result file and return
-        fs.readFile(tempFileOutput, 'utf8', function (err, data) {
+        fs.readFile(tempFileOutput, 'utf8', (err, data) => {
           if (err) {
             def.reject('Unable to read ' + tempFileOutput)
             return
@@ -97,7 +99,7 @@ function runCluster (data, config, opts) {
       }
 
       // remove all related files
-      fs.remove(tempFileBase + '.*', function (err) {
+      fs.remove(tempFileBase + '.*', (err) => {
         if (err) return debug(err)
 
         debug('success removed temp files: ', tempFileBase + '.*')
@@ -110,20 +112,23 @@ function runCluster (data, config, opts) {
 
 module.exports = {
   requireAuthentication: false,
-  validate: function (params, req, res) {
+  validate: (params, req, res) => {
     // validate parameters here
     return params && params[0]
   },
-  execute: function (params, req, res) {
+  execute: (params, req, res) => {
+    // allow enough time for clustering to complete (or be killed)
+    res.setTimeout(1000 * 60 * 5.25)
+
     const def = new Deferred()
 
     const data = params[0]
     const config = params[1]
-    const opts = {req: req, user: req.user}
+    const opts = { req: req, user: req.user }
 
-    when(runCluster(data, config, opts), function (result) {
+    when(runCluster(data, config, opts), (result) => {
       def.resolve(result)
-    }, function (err) {
+    }, (err) => {
       def.reject('Unable to Complete Cluster: ' + err)
     })
 
