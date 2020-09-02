57a4475ff254afd0fdc40c26d8be977ab44bf1e6
Harry Yoo <hsyoo@anl.gov>
2020-09-02 17:29:08 -0500

    code style update. syntax fix for async evaluation

--- tests/test-jbrowse/test.spec.js
+++ tests/test-jbrowse/test.spec.js
@@ -1,17 +1,17 @@
 const assert = require('chai').assert
-const http = require('http')
-const {httpGet} = require('../../util/http')
+const Http = require('http')
+const { httpGet } = require('../../util/http')
 const Path = require('path')
-const fs = require('fs')
-const config = require('../../config')
+const Fs = require('fs')
+const Config = require('../../config')
 
-const agent = new http.Agent({
+const agent = new Http.Agent({
   keepAlive: true,
   maxSockets: 1
 })
 
 const rqlRequestOptions = {
-  port: config.get('http_port'),
+  port: Config.get('http_port'),
   agent: agent,
   headers: {
     'Accept': 'application/json',
@@ -19,138 +19,101 @@ const rqlRequestOptions = {
   }
 }
 
-describe('Test Router - JBrowse', () => {
-
-  const ExpectedTrackList = fs.readFileSync(Path.join(__dirname, 'expected.trackList.json'), {
+describe('Test Router - JBrowse', function () {
+  const ExpectedTrackList = Fs.readFileSync(Path.join(__dirname, 'expected.trackList.json'), {
     encoding: 'utf8'
   })
-  it('GET /jbrowse/genome/:id/trackList', function (done) {
-    (async () => {
-      try {
-        const body = await httpGet(Object.assign(rqlRequestOptions, {
-          path: '/jbrowse/genome/83332.12/trackList'
-        }))
+  it('GET /jbrowse/genome/:id/trackList', async function () {
+    return httpGet(Object.assign(rqlRequestOptions, {
+      path: '/jbrowse/genome/83332.12/trackList'
+    }))
+      .then((body) => {
         assert.equal(body, ExpectedTrackList)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })()
+      })
   })
 
-  const ExpectedTracks = fs.readFileSync(Path.join(__dirname, 'expected.tracks.json'), {
+  const ExpectedTracks = Fs.readFileSync(Path.join(__dirname, 'expected.tracks.json'), {
     encoding: 'utf8'
   })
-  it('GET /genome/:id/tracks', function (done) {
-    (async () => {
-      try {
-        const body = await httpGet(Object.assign(rqlRequestOptions, {
-          path: '/jbrowse/genome/83332.12/tracks'
-        }))
+  it('GET /genome/:id/tracks', async function () {
+    return httpGet(Object.assign(rqlRequestOptions, {
+      path: '/jbrowse/genome/83332.12/tracks'
+    }))
+      .then((body) => {
         assert.equal(body, ExpectedTracks)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })()
-  })
-  
-  const ExpectedStatsGlobal = fs.readFileSync(Path.join(__dirname, 'expected.StatsGlobal.json'), {
-    encoding: 'utf8'
-  })
-  it('GET /jbrowse/genome/:id/stats/global', function (done) {
-    (async () => {
-      try {
-        const body = await httpGet(Object.assign(rqlRequestOptions, {
-          path: '/jbrowse/genome/83332.12/stats/global'
-        }))
-        assert.equal(body, ExpectedStatsGlobal)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })()
+      })
   })
 
-  const ExpectedStatsRegion = fs.readFileSync(Path.join(__dirname, 'expected.StatsRegion.json'), {
+  const ExpectedStatsGlobal = Fs.readFileSync(Path.join(__dirname, 'expected.StatsGlobal.json'), {
     encoding: 'utf8'
   })
-  it('GET /jbrowse/genome/:id/stats/region/:sequence_id', function (done) {
-    (async () => {
-      try {
-        const body = await httpGet(Object.assign(rqlRequestOptions, {
-          path: '/jbrowse/genome/83332.12/stats/region/NC_000962'
-        }))
+  it('GET /jbrowse/genome/:id/stats/global', async function () {
+    return httpGet(Object.assign(rqlRequestOptions, {
+      path: '/jbrowse/genome/83332.12/stats/global'
+    }))
+      .then((body) => {
+        assert.equal(body, ExpectedStatsGlobal)
+      })
+  })
+
+  const ExpectedStatsRegion = Fs.readFileSync(Path.join(__dirname, 'expected.StatsRegion.json'), {
+    encoding: 'utf8'
+  })
+  it('GET /jbrowse/genome/:id/stats/region/:sequence_id', async function () {
+    return httpGet(Object.assign(rqlRequestOptions, {
+      path: '/jbrowse/genome/83332.12/stats/region/NC_000962'
+    }))
+      .then((body) => {
         assert.equal(body, ExpectedStatsRegion)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })()
+      })
   })
 
   // DEPRECATED
-  // const ExpectedStatsRegionFeatureDensities = fs.readFileSync(Path.join(__dirname, 'expected.StatsRegionFeatureDensities.json'), {
+  // const ExpectedStatsRegionFeatureDensities = Fs.readFileSync(Path.join(__dirname, 'expected.StatsRegionFeatureDensities.json'), {
   //   encoding: 'utf8'
   // })
-  // it('GET /genome/:id/stats/regionFeatureDensities/:sequence_id', function (done) {
-  //   (async () => {
-  //     const body = await httpGet(Object.assign(rqlRequestOptions, {
-  //       path: '/genome/83332.12/stats/regionFeatureDensities/NC_000962?annotation=PATRIC&start=0&end=99999&basesPerBin=10000'
-  //     }))
-  //     assert.equal(body, ExpectedStatsRegionFeatureDensities)
-  //     done()
-  //   })()
+  // it('GET /genome/:id/stats/regionFeatureDensities/:sequence_id', async function () {
+  //   return httpGet(Object.assign(rqlRequestOptions, {
+  //     path: '/genome/83332.12/stats/regionFeatureDensities/NC_000962?annotation=PATRIC&start=0&end=99999&basesPerBin=10000'
+  //   }))
+  //     .then((body) => {
+  //       assert.equal(body, ExpectedStatsRegionFeatureDensities)
+  //     })
   // })
 
-  const ExpectedFeatures = fs.readFileSync(Path.join(__dirname, 'expected.features.json'), {
+  const ExpectedFeatures = Fs.readFileSync(Path.join(__dirname, 'expected.features.json'), {
     encoding: 'utf8'
   })
-  it('GET /jbrowse/genome/:id/features/:seq_accession?annotation=:annotation&start=:start&end=:end', function (done) {
-    (async () => {
-      try {
-        const body = await httpGet(Object.assign(rqlRequestOptions, {
-          path: '/jbrowse/genome/83332.12/features/NC_000962?annotation=PATRIC&start=0&end=99999'
-        }))
+  it('GET /jbrowse/genome/:id/features/:seq_accession?annotation=:annotation&start=:start&end=:end', async function () {
+    return httpGet(Object.assign(rqlRequestOptions, {
+      path: '/jbrowse/genome/83332.12/features/NC_000962?annotation=PATRIC&start=0&end=99999'
+    }))
+      .then((body) => {
         assert.equal(body, ExpectedFeatures)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })()
+      })
   })
 
-  const ExpectedRefseq = fs.readFileSync(Path.join(__dirname, 'expected.refseqs.json'), {
+  const ExpectedRefseq = Fs.readFileSync(Path.join(__dirname, 'expected.refseqs.json'), {
     encoding: 'utf8'
   })
-  it('GET /jbrowse/genome/:id/refseqs', function (done) {
-    (async () => {
-      try {
-        const body = await httpGet(Object.assign(rqlRequestOptions, {
-          path: '/jbrowse/genome/83332.12/refseqs'
-        }))
+  it('GET /jbrowse/genome/:id/refseqs', async function () {
+    return httpGet(Object.assign(rqlRequestOptions, {
+      path: '/jbrowse/genome/83332.12/refseqs'
+    }))
+      .then((body) => {
         assert.equal(body, ExpectedRefseq)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })()
+      })
   })
 
-  const ExpectedNames = fs.readFileSync(Path.join(__dirname, 'expected.names.json'), {
+  const ExpectedNames = Fs.readFileSync(Path.join(__dirname, 'expected.names.json'), {
     encoding: 'utf8'
   })
-  it('GET /jbrowse/genome/:id/names/', function (done) {
-    (async () => {
-      try {
-        const body = await httpGet(Object.assign(rqlRequestOptions, {
-          path: '/jbrowse/genome/83332.12/names/'
-        }))
+  it('GET /jbrowse/genome/:id/names/', async function () {
+    return httpGet(Object.assign(rqlRequestOptions, {
+      path: '/jbrowse/genome/83332.12/names/'
+    }))
+      .then((body) => {
         assert.equal(body, ExpectedNames)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })()
+      })
   })
 })
