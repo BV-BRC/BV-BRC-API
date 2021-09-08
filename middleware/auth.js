edef1774ea609f76a78105de2eabfeab4fb18238
Dustin Machi <dmachi@virginia.edu>
2021-09-08 15:33:57 -0400

    pass expected signingSubjectURL to validator

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,14 @@
 const ValidateToken = require('p3-user/validateToken')
+var config = require('../config')
+const signingSubjectURL = config.get("signingSubjectURL");
 
 module.exports = function (req, res, next) {
+  if (!signingSubjectURL){
+    return next(new Error("Missing signingSubjectURL in config"))
+  }
   if (!req.isAuthenticated || (req.isAuthenticated && !req.isAuthenticated())) {
     if (req.headers && req.headers['authorization']) {
-      ValidateToken(req.headers['authorization'])
+      ValidateToken(req.headers['authorization'], signingSubjectURL)
         .then((valid) => {
           if (valid && valid.id) {
             req.user = valid.id
