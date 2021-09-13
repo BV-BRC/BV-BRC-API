8294a730c5e6d2f67b8e7e493aeef87acdc525bf
Harry Yoo <hsyoo@anl.gov>
2021-09-13 13:42:19 -0500

    fix eslint errors

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
 const ValidateToken = require('p3-user/validateToken')
 var config = require('../config')
-const signingSubjectURL = config.get("signingSubjectURL");
+const signingSubjectURL = config.get('signingSubjectURL')
 
 module.exports = function (req, res, next) {
-  if (!signingSubjectURL){
-    return next(new Error("Missing signingSubjectURL in config"))
+  if (!signingSubjectURL) {
+    return next(new Error('Missing signingSubjectURL in config'))
   }
   if (!req.isAuthenticated || (req.isAuthenticated && !req.isAuthenticated())) {
     if (req.headers && req.headers['authorization']) {
