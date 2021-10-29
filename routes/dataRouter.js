045a9369f347ae94f8a502b499101888d18c5e0e
Harry Yoo <hsyoo@anl.gov>
2021-10-29 16:28:55 -0500

    add epitope_type

--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -116,6 +116,7 @@ router.get('/summary_by_taxon/:taxon_id', [
   }
 ])
 const allowed = {
+  'epitope': ['epitope_type'],
   'genome': ['host_group', 'host_name', 'host_common_name', 'geographic_group', 'isolation_country'],
   'genome_feature': ['feature_type'],
   'sp_gene': ['property', 'source', 'evidence'],
